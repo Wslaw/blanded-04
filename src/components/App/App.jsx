e14cf72f14b18b823c7420634ff51fc0ba48f2d7
TetianaKubrak73 <tetianakubrak73@gmail.com>
2024-02-17 11:54:56 +0100

    2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { Component, useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
-
+import { addTodo, getTodos, deleteTodo } from 'store/todoSlice';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   Container,
   Grid,
@@ -12,71 +13,64 @@ import {
   Todo,
 } from 'components';
 
-export const App =  () => {
-  const [todos, setTodos] = useState(() => {
+export const App = () => {
+  // const [todos, setTodos] = useState(() => {
+  //   return JSON.parse(localStorage.getItem('todos')) || [];
+  // });
 
-    return JSON.parse(localStorage.getItem('todos')  )  || [];
-  });
-
-
-
+  const dispatch = useDispatch();
+  const todos = useSelector(getTodos);
   useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify(todos));
-  },[todos])
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
-
-  const addTodo = text => {
+  const addnewTodo = text => {
     const todo = {
       id: nanoid(),
       text,
     };
-    
-setTodos(( prevTodos ) => (
-       [...prevTodos, todo]
-    ));
+
+    // setTodos(( prevTodos ) => (
+    //        [...prevTodos, todo]
+    //     ));
+    dispatch(addTodo(todo));
   };
-
-
-
 
   const handleSubmit = data => {
-    addTodo(data);
+    addnewTodo(data);
   };
 
-  const deleteTodo = id => {
-    setTodos(prevState => (
-     prevState.filter(todo => todo.id !== id)
-    ));
+  const deleteNewTodo = id => {
+    // setTodos(prevState => prevState.filter(todo => todo.id !== id));
+    dispatch(deleteTodo(id));
   };
 
-   
-    return (
-      <>
-        <Header />
-        <Section>
-          <Container>
-            <SearchForm onSubmit={handleSubmit} />
+  return (
+    <>
+      <Header />
+      <Section>
+        <Container>
+          <SearchForm onSubmit={handleSubmit} />
 
-            {todos.length === 0 && (
-              <Text textAlign="center">There are no any todos ... </Text>
-            )}
+          {todos.length === 0 && (
+            <Text textAlign="center">There are no any todos ... </Text>
+          )}
 
-            <Grid>
-              {todos.length > 0 &&
-                todos.map((todo, index) => (
-                  <GridItem key={todo.id}>
-                    <Todo
-                      id={todo.id}
-                      text={todo.text}
-                      counter={index + 1}
-                      onClick={deleteTodo}
-                    />
-                  </GridItem>
-                ))}
-            </Grid>
-          </Container>
-        </Section>
-      </>
-    );
-  }
-
+          <Grid>
+            {todos.length > 0 &&
+              todos.map((todo, index) => (
+                <GridItem key={todo.id}>
+                  <Todo
+                    id={todo.id}
+                    text={todo.text}
+                    counter={index + 1}
+                    onClick={deleteNewTodo}
+                  />
+                </GridItem>
+              ))}
+          </Grid>
+        </Container>
+      </Section>
+    </>
+  );
+};
--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -1,13 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    todos:[],
-}
+  todos: [],
+};
 
 const todosSlice = createSlice({
-    name: 'todos',
-    initialState,
-    reducers:{},
-})
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push(action.payload);
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter(item => item.id !== action.payload);
+    },
+  },
+});
 
 export default todosSlice.reducer;
+export const { addTodo, deleteTodo } = todosSlice.actions;
+export const getTodos = state => state.todos;
